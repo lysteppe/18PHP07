6cbdd05501db01ee86a269faaedbc5206f3ea544
Nguyên <38059005+lysteppe@users.noreply.github.com>
2018-12-17 00:35:26 +0700

    Update form.js

--- Example/form.js
+++ Example/form.js
@@ -4,15 +4,6 @@ function show() {
 	var name= document.getElementById('Fullname').value;
 	document.getElementById('fullname').innerHTML = name;
 	
-	var male= document.getElementById('male').checked;
-	var female= document.getElementById('female').checked;
-		if (male) {
-			document.getElementById('show-img').src="male.jpg";
-		}
-		else if (female) {
-			document.getElementById('show-img').src="female.jpg";
-		}
-
 	var yearr= document.getElementById('yearr').value;
 	document.getElementById('year').innerHTML = yearr;
 	if (yearr > 1998) {
@@ -21,4 +12,13 @@ function show() {
 	else if (yearr < 1998) {
 		document.getElementById('show').style.background = ('green');
 	}
+	
+	var male= document.getElementById('male').checked;
+	var female= document.getElementById('female').checked;
+		if (male) {
+			document.getElementById('show-img').src="male.jpg";
+		}
+		else if (female) {
+			document.getElementById('show-img').src="female.jpg";
+		}
 }
